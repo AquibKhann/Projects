16250dfe27582fcd1183949d9e6506ea558a09a1
Aquib Khan <aquibkhanstud@gmail.com>
2024-02-07 22:44:47 +0530

    added home rout

--- Database/index.js
+++ Database/index.js
@@ -1,5 +1,11 @@
 const { faker } = require("@faker-js/faker");
 const mysql = require('mysql2');
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -7,24 +13,35 @@ const connection = mysql.createConnection({
     database: 'My_App',
     password: 'Kingkhan@mysql',
   });
-try{
-    connection.query("SHOW TABLES" , (err , result) => {
+
+  let  getRandomUser = () => {
+    return [
+       faker.datatype.uuid(),
+       faker.internet.userName(),
+       faker.internet.email(),
+       faker.internet.password(),
+    ];
+  };
+
+
+
+app.get("/", (req,res)=> {
+  let q = `SELECT count(*) FROM user`;
+  try{
+    connection.query(q, (err , result) => {
         if(err) throw err;
-        console.log(result);
+        let count =result[0]["count(*)"];
+        res.render("home.ejs",{count});
     });
 }
     catch (err){
     console.log(err);
+    res.send("Some error in database");
 }
-connection.end();
+});
+
+app.listen("8080", ()=>{
+  console.log("Server is listening to port 8080");
+});
 
 
-let  getRandomUser = () => {
-    return {
-      Id: faker.string.uuid(),
-      username: faker.internet.userName(),
-      email: faker.internet.email(),
-      avatar: faker.image.avatar(),
-      password: faker.internet.password()
-    };
-  };
